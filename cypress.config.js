c1f7e3b791ddab81e771b3f4c86ec2f7123d8726
avirajBoxture <107924052+abundantAvi@users.noreply.github.com>
2023-09-16 22:10:59 +0530

    Add artifacts

--- cypress.config.js
+++ cypress.config.js
@@ -11,6 +11,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern: "cypress/e2e/exploratory/filters.cy.js",
+    
   },
 });
