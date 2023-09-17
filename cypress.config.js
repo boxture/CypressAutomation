58edcc05c592940bb38756339bea613ec3375131
avirajBoxture <107924052+abundantAvi@users.noreply.github.com>
2023-09-17 12:37:35 +0530

    retries

--- cypress.config.js
+++ cypress.config.js
@@ -6,11 +6,14 @@ module.exports = defineConfig({
   viewportHeight: 900,
   defaultCommandTimeout: 10000,
   watchForFileChanges: false,
-
+  retries: {
+    runMode: 3,
+    openMode: 3,
+  },
   e2e: {
     baseUrl: "https://oms.staging.boxture.com",
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+      require("cypress-mochawesome-reporter/plugin")(on);
       // implement node event listeners here
     },
   },
