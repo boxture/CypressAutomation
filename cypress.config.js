0fdaf0bc34b5c018b91614132004e03ce1690d7a
avirajBoxture <107924052+abundantAvi@users.noreply.github.com>
2023-09-21 07:33:07 +0530

    mochareport

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,17 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "xz6ech",
+  reporter: "cypress-mochawesome-reporter",
   viewportWidth: 1600,
   viewportHeight: 900,
+  defaultCommandTimeout: 10000,
+  watchForFileChanges: false,
   e2e: {
-    baseUrl: 'https://oms.staging.boxture.com',
+    baseUrl: "https://oms.staging.boxture.com",
     setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
+      // implement node event listeners here
       // implement node event listeners here
     },
   },
