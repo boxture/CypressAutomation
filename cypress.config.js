ec38c688df326d4ea9cd36c8a5d7a57e6a264137
avirajBoxture <107924052+abundantAvi@users.noreply.github.com>
2023-09-22 10:14:43 +0530

    changes added

--- cypress.config.js
+++ cypress.config.js
@@ -6,20 +6,21 @@ module.exports = defineConfig({
   viewportWidth: 1600,
   viewportHeight: 900,
   defaultCommandTimeout: 10000,
-  video: true,
-  watchForFileChanges: false,
+  video: false,
+  // videoRecording: false,
+  // watchForFileChanges: false,
   retries: {
     runMode: 2,
   },
   e2e: {
     baseUrl: "https://oms.staging.boxture.com",
     setupNodeEvents(on, config) {
-      on("task", {});
-      on("after:spec", (spec, results) => {
-        if (results.stats.failures === 0 && results.video) {
-          return del(results.video);
-        }
-      });
+      // on("task", {});
+      // on("after:spec", (spec, results) => {
+      //   if (results.stats.failures === 0 && results.video) {
+      //     return del(results.video);
+      //   }
+      // });
       require("cypress-mochawesome-reporter/plugin")(on);
       // implement node event listeners here
       // implement node event listeners here
